ac06e62e788cd49ec1a2e634dd7b6d96ec1d57db
agris <33702214+agrism@users.noreply.github.com>
2023-09-12 21:57:31 +0300

    remove \n

--- lib/Minify/HTML.php
+++ lib/Minify/HTML.php
@@ -152,7 +152,7 @@ class Minify_HTML
         );
 
         // use newlines before 1st attribute in open tags (to limit line lengths)
-        $this->_html = preg_replace('/(<[a-z\\-]+)\\s+([^>]+>)/iu', "$1\n$2", $this->_html);
+        $this->_html = preg_replace('/(<[a-z\\-]+)\\s+([^>]+>)/iu', "$1 $2", $this->_html);
 
         // fill placeholders
         $this->_html = str_replace(
